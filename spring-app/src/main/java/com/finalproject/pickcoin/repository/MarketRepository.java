d3404b9b3ee171f645c36536c1b38c7c6725fc33
hji107 <hji107@naver.com>
2025-08-06 18:10:33 +0900

    250806 db-프론트 연동 진행중

---
+++ spring-app/src/main/java/com/finalproject/pickcoin/repository/MarketRepository.java
@@ -0,0 +1,11 @@
+package com.finalproject.pickcoin.repository;
+import com.finalproject.pickcoin.domain.Asset;
+import org.apache.ibatis.annotations.Mapper;
+import java.util.List;
+
+@Mapper
+public interface MarketRepository {
+
+        
+    public List<Asset> get_coin_list();
+}
