06135adedf23bbcd4d9fb462155cde7f7b45ca60
bag jeesoo <jeesoo1899@gmail.com>
2025-08-08 16:32:38 +0900

    1:1 문의관리 디비값 출력(메세지는 따로 구현해야함)

--- spring-app/src/main/java/com/finalproject/pickcoin/domain/Inquiry.java
+++ spring-app/src/main/java/com/finalproject/pickcoin/domain/Inquiry.java
@@ -2,6 +2,7 @@ package com.finalproject.pickcoin.domain;
 
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Column;
@@ -13,21 +14,23 @@ import lombok.Data;
 
 @Entity
 @Data
-public class Inquriy {
+public class Inquiry {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer inquiry_id;
+    private Integer user_id;
     private String category;
     private Double amount;
+
+    @ColumnDefault("'신규'")
     private String status;
-    private Integer priority;
 
     @Column(name="created_at", updatable = false)
     @CreationTimestamp
     private LocalDateTime createdAt;
 
-    @Column(name="closed_at", updatable = false)
+    @Column(name="closed_at")
     private LocalDateTime closedAt;
 
 }
