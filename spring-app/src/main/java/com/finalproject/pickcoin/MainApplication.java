bf0e06f194ce36f9bae65963b80350d7027e54db
LHS <shl-sys@users.noreply.github.com>
2025-08-08 10:44:37 +0900

    오류수정본_250808

--- spring-app/src/main/java/com/finalproject/pickcoin/MainApplication.java
+++ spring-app/src/main/java/com/finalproject/pickcoin/MainApplication.java
@@ -1,29 +1,16 @@
 package com.finalproject.pickcoin;
 
-<<<<<<< HEAD
 import org.mybatis.spring.annotation.MapperScan;
-=======
->>>>>>> feature_jh
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-<<<<<<< HEAD
-@MapperScan("com.finalproject.pickcoin.mapper") // mapper 인터페이스 패키지 경로
-=======
->>>>>>> feature_jh
+// @MapperScan("com.finalproject.pickcoin.mapper") 
 @SpringBootApplication
 @EnableScheduling
 public class MainApplication {
 
-<<<<<<< HEAD
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
     }
-=======
-	public static void main(String[] args) {
-		SpringApplication.run(MainApplication.class, args);
-	}
-
->>>>>>> feature_jh
 }
