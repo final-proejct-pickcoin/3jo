20f4e0769aef15dc19c7835f41e8f29ba0fd1fd9
LHS <shl-sys@users.noreply.github.com>
2025-08-29 12:19:40 +0900

    url변경 merge전 푸쉬_feature_l

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -64,7 +64,7 @@ app = FastAPI(
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
-host = 'mysql'
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 templates = Jinja2Templates(directory="templates")
